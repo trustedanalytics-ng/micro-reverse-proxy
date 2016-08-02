d9b1c7b878a342d0c22a320a7ded8201e4e0333b
Artur Ignatowski <artur.ignatowski@intel.com>
2016-08-02 08:38:51 +0200

    Refactoring, decrease internal modules methods scope. Expose the only methods used on niginx configuration level

--- libs/tap-auth.lua
+++ libs/tap-auth.lua
@@ -6,8 +6,9 @@ local uid = os.getenv("USER_ID")
 assert(uid ~= nil, "Environment variable USER_ID not set")
 
 local M = {}
+local private = {}
 
-function M.getCACert(cacert_location) 
+local function getCACert(cacert_location) 
   local pkey = os.getenv("JWT_PUBLIC_KEY")
   if pkey == nil then
     local f = io.open(cacert_location, "rb")
@@ -17,7 +18,7 @@ function M.getCACert(cacert_location)
   return pkey
 end
 
-function M.auth()
+local function auth()
   local auth_header = ngx.var.http_Authorization
   if auth_header == nil then
     ngx.log(ngx.INFO, "No Authorization header")
@@ -29,36 +30,58 @@ function M.auth()
       ngx.log(ngx.WARN, "Invalid authorization header - missing token")
       ngx.exit(ngx.HTTP_UNAUTHORIZED)
   end
-  if not M.verify(public_key, token) then
+
+  if not private.verify(public_key, token) then
       ngx.log(ngx.WARN, "Authorization token not verified")
       ngx.exit(ngx.HTTP_UNAUTHORIZED)
   end
-
-  local exit_code = M.ktinit(token)
-  if not exit_code == 0 then
-      ngx.log(ngx.WARN, "Can't get kerberos ticket")
+  
+  local status, err = pcall(private.session, token)
+  if not status then
+      ngx.log(ngx.WARN, "Can't get kerberos ticket: " .. err)
       ngx.exit(ngx.HTTP_UNAUTHORIZED)
   end  
 end  
 
-function M.verify(public_key, token)
+function private.verify(public_key, token)
   jwt:set_alg_whitelist({RS256 = 1})
-  local jwt_obj = jwt:verify(public_key, token, M.claims())
+  local jwt_obj = jwt:verify(public_key, token, private.claims())
   table.foreach(jwt_obj, function(k,v) ngx.log(ngx.INFO, tostring(k) .. "=>" .. tostring(v)) end)
   return jwt_obj.verified
 end
 
-function M.claims()
+function private.claims()
   return {
     exp = validators.is_not_expired(),
     user_id = validators.equals(uid)
   }
 end 
 
-function M.ktinit(token) 
+function private.ktinit(token) 
   local cmd = string.format("ktinit -t %s -c %s", token, "/tmp/krb5cc")
-  ngx.log(ngx.INFO, cmd)
-  return os.execute(cmd)
+  local exit_code =  os.execute(cmd)
+  if exit_code ~= 0 then 
+     error(string.format("Execution failed: %s, [exit code: %02d]", cmd, exit_code))
+  else  
+     return exit_code
+  end
 end
 
+function private.session(token)
+  local session_id  = ngx.var.cookie_session;
+  if session_id == nil then
+    local expires = 60 * 5 -- five minutes
+    local status, err = private.ktinit(token)
+    --TODO generate session id
+    session_id = uid
+    ngx.header["Set-Cookie"] = 
+       string.format("session=%s; Path=/; Expires=%s", 
+                      session_id, 
+                      ngx.cookie_time(ngx.time() + expires))
+  end
+  return session_id
+end 
+
+M.getCACert = getCACert
+M.auth = auth
 return M
